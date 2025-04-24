94946f39caf0fa06b4761168f16c3565c1d611e2
DevCodeMathias <lauramathias03@gmail.com>
2025-04-24 00:48:37 -0300

    up project

---
+++ src/main/java/com/logger/logger_api/LoggerApiApplication.java
@@ -0,0 +1,13 @@
+package com.logger.logger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logger/logger_api/LoggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logger.logger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
